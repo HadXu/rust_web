2823a01a27ab24a7de04b2e5599f16d4f3e93926
lei <hadxu123@gmail.com>
2022-01-07 21:13:52 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use std::net::SocketAddr;
+
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Method, Request, Response, Server, StatusCode,
+};
+
+use std::fs;
+
+use futures::TryStreamExt as _;
+
+use anyhow::{Error, Result};
+
+#[tokio::main]
+async fn main() {
+    // We'll bind to 127.0.0.1:3000
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = make_service_fn(|_conn| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, hyper::Error>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Error> {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            let content = fs::read_to_string("rust.md")?;
+
+            *response.body_mut() = Body::from(content);
+        }
+
+        (&Method::GET, "/pdf") => {
+            response
+                .headers_mut()
+                .insert("Content-type", "application/pdf".parse()?);
+            let content = fs::read("xian.pdf")?;
+            *response.status_mut() = StatusCode::OK;
+            *response.body_mut() = Body::from(content);
+        }
+
+        (&Method::GET, "/html") => {
+            response
+                .headers_mut()
+                .insert("Content-type", "text/html".parse()?);
+            *response.body_mut() = Body::from("<h1>xxxx</h1>");
+        }
+
+        (&Method::GET, "/jpg") => {
+            response.headers_mut().insert("Content-type", "".parse()?);
+            let content = fs::read("rhesus.png")?;
+            *response.status_mut() = StatusCode::OK;
+            *response.body_mut() = Body::from(content);
+        }
+
+        (&Method::POST, "/echo") => *response.body_mut() = req.into_body(),
+        (&Method::POST, "/echo/uppercase") => {
+            let mapping = req.into_body().map_ok(|chunk| {
+                chunk
+                    .iter()
+                    .map(|byte| byte.to_ascii_uppercase())
+                    .collect::<Vec<u8>>()
+            });
+
+            *response.body_mut() = Body::wrap_stream(mapping);
+        }
+
+        (&Method::POST, "/echo/reverse") => {
+            let full_body = hyper::body::to_bytes(req.into_body()).await?;
+            let reversed = full_body.iter().rev().cloned().collect::<Vec<u8>>();
+
+            *response.body_mut() = reversed.into();
+        }
+
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    }
+    Ok(response)
+}
